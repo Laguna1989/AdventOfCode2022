e44e9299ee6a4e093c591d202a855526512eae30
Thunraz <thunraz@thunraz.com>
2022-12-28 14:39:32 +0100

    Implement first exercise

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!{:?}", 1);
+    let file_path = "./example.txt";
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    let mut max_calories_per_elf : u32 = 0;
+
+    let split_content = contents.split("\n\n");
+    for calories_per_elf in split_content {
+        let mut total_calories_per_elf : u32 = 0;
+        
+        for calory_str in calories_per_elf.trim().split("\n") {
+            let calory = calory_str.trim().parse::<u32>();
+            total_calories_per_elf += calory.unwrap();
+        }
+
+        if total_calories_per_elf >= max_calories_per_elf {
+            max_calories_per_elf = total_calories_per_elf;
+        }
+    }
+
+    println!("Max calories per Elf:\n{max_calories_per_elf}");
 }
