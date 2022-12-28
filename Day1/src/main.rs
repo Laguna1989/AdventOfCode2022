b55d62867ec1aab822f1f7816a3b237692c7ede5
Simon Weis <laguna_1989@gmx.net>
2022-12-28 15:20:26 +0100

    improve solution

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,27 +1,17 @@
-use std::fs;
-
 fn parse_and_unwrap(calory_str: &str) -> u32 {
     calory_str.parse::<u32>().unwrap()
 }
 
 fn main() {
-    let file_path = "./example.txt";
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = include_str!("../example.txt");
 
     let mut vec_max_calories_per_elf: Vec<u32> = contents.split("\n\n")
         .map(|calories_per_elf| calories_per_elf
-            .trim().split("\n")
+            .lines()
             .map(|calory_str| parse_and_unwrap(calory_str))
             .sum())
         .collect();
+    vec_max_calories_per_elf.sort_unstable();
 
-    vec_max_calories_per_elf.sort();
-    vec_max_calories_per_elf.reverse();
-
-    let top3_calories = vec_max_calories_per_elf[0] + vec_max_calories_per_elf[1] + vec_max_calories_per_elf[2];
-
-    println!("Top 3 calories:\n{top3_calories}");
+    println!("Top 3 calories:\n{}", vec_max_calories_per_elf.iter().rev().take(3).sum::<u32>());
 }
