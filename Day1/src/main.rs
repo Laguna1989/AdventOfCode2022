b3f109447b9f26f1f7d9fd878c8bb4fb743f5e0e
Simon Weis <laguna_1989@gmx.net>
2022-12-28 14:57:00 +0100

    play around with fancy rust features

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,5 +1,9 @@
 use std::fs;
 
+fn parse_and_unwrap(calory_str: &str) -> u32 {
+    calory_str.parse::<u32>().unwrap()
+}
+
 fn main() {
     let file_path = "./example.txt";
     println!("In file {}", file_path);
@@ -7,21 +11,15 @@ fn main() {
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    let mut max_calories_per_elf : u32 = 0;
-
-    let split_content = contents.split("\n\n");
-    for calories_per_elf in split_content {
-        let mut total_calories_per_elf : u32 = 0;
-        
-        for calory_str in calories_per_elf.trim().split("\n") {
-            let calory = calory_str.trim().parse::<u32>();
-            total_calories_per_elf += calory.unwrap();
-        }
-
-        if total_calories_per_elf >= max_calories_per_elf {
-            max_calories_per_elf = total_calories_per_elf;
-        }
-    }
+    let max_calories_per_elf: u32 = contents.split("\n\n")
+        .map(|calories_per_elf| calories_per_elf
+            .trim().split("\n")
+            .map(|calory_str| parse_and_unwrap(calory_str))
+            .sum())
+        .max()
+        .unwrap();
 
     println!("Max calories per Elf:\n{max_calories_per_elf}");
 }
+
+
