784b1f27174d83922cd27baa69a3d7d4f9d56b24
Simon Weis <laguna.ws@gmail.com>
2023-09-26 22:22:37 +0200

    Add solution for day4 b

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,5 +1,3 @@
-use std::ops::Range;
-
 struct ElvesPair {
     a_l: i32,
     a_u: i32,
@@ -8,44 +6,21 @@ struct ElvesPair {
     b_u: i32,
 }
 
-fn bounds_from_string(single_elf_input: &str) -> (i32, i32)
-{
-    let split = &single_elf_input.split("-").collect::<Vec<_>>();
-    let lower: i32 = split[0].parse().unwrap();
-    let upper: i32 = 1 + split[1].parse::<i32>().unwrap();
-    (lower, upper)
-}
-
 impl ElvesPair {
     fn new(input: &str) -> Self {
-        let split = &input.split(",").collect::<Vec<_>>();
-        let first_elf_bounds = bounds_from_string(split[0]);
-        let second_elf_bounds = bounds_from_string(split[1]);
+        let (first, second) = &input.split_once(",").unwrap();
+        let ((a_l, a_u), (b_l, b_u)) = (first.split_once('-').unwrap(), second.split_once('-').unwrap());
 
         Self {
-            a_l: first_elf_bounds.0,
-            a_u: first_elf_bounds.1,
-            b_l: second_elf_bounds.0,
-            b_u: second_elf_bounds.1,
+            a_l: a_l.parse::<i32>().unwrap(),
+            a_u: a_u.parse::<i32>().unwrap(),
+            b_l: b_l.parse::<i32>().unwrap(),
+            b_u: b_u.parse::<i32>().unwrap(),
         }
     }
 
-    pub fn check_for_full_overlap(&self) -> bool {
-        // range a < range b
-        if self.a_l >= self.b_l && self.a_u <= self.b_u {
-            return true;
-        }
-        // range b < range a
-        if self.b_l >= self.a_l && self.b_u <= self.a_u {
-            return true;
-        }
-
-        // range a == range b
-        if self.a_l == self.b_l && self.a_u == self.b_u {
-            return true;
-        }
-
-        false
+    pub fn check_for_any_overlap(&self) -> bool {
+        return self.a_l <= self.b_u && self.b_l <= self.a_u;
     }
 }
 
@@ -53,7 +28,7 @@ impl ElvesPair {
 fn main() {
     let contents = include_str!("../puzzle_input.txt");
     let elves = contents.lines().map(|l| ElvesPair::new(l));
-    let count = elves.filter(|ep| ep.check_for_full_overlap()).count();
+    let count = elves.filter(|ep| ep.check_for_any_overlap()).count();
     println!("elf pairs with full overlap: {}", count);
 }
 
@@ -64,31 +39,67 @@ mod test {
     #[test]
     fn elves_pair_no_overlap() {
         let ep = ElvesPair::new("1-1,2-2");
-        assert_eq!(ep.check_for_full_overlap(), false);
+        assert_eq!(ep.check_for_any_overlap(), false);
     }
 
     #[test]
     fn elves_pair_full_overlap_1() {
         let ep = ElvesPair::new("1-1,1-1");
-        assert_eq!(ep.check_for_full_overlap(), true);
+        assert_eq!(ep.check_for_any_overlap(), true);
     }
 
     #[test]
     fn elves_pair_full_overlap_2() {
         let ep = ElvesPair::new("4-110,4-110");
-        assert_eq!(ep.check_for_full_overlap(), true);
+        assert_eq!(ep.check_for_any_overlap(), true);
     }
 
     #[test]
     fn elves_pair_partial_overlap_1() {
         let ep = ElvesPair::new("1-5,4-6");
-        assert_eq!(ep.check_for_full_overlap(), false);
+        assert_eq!(ep.check_for_any_overlap(), true);
     }
 
     #[test]
     fn elves_pair_completely_enclosed_overlap() {
         let ep = ElvesPair::new("1-5,3-4");
-        assert_eq!(ep.check_for_full_overlap(), true);
+        assert_eq!(ep.check_for_any_overlap(), true);
+    }
+
+    #[test]
+    fn example_overlap_1() {
+        let ep = ElvesPair::new("5-7,7-9");
+        assert_eq!(ep.check_for_any_overlap(), true);
+    }
+
+    #[test]
+    fn example_overlap_2() {
+        let ep = ElvesPair::new("2-8,3-7");
+        assert_eq!(ep.check_for_any_overlap(), true);
+    }
+
+    #[test]
+    fn example_overlap_3() {
+        let ep = ElvesPair::new("6-6,4-6");
+        assert_eq!(ep.check_for_any_overlap(), true);
+    }
+
+    #[test]
+    fn example_overlap_4() {
+        let ep = ElvesPair::new("2-6,4-8");
+        assert_eq!(ep.check_for_any_overlap(), true);
+    }
+
+    #[test]
+    fn example_overlap_5() {
+        let ep = ElvesPair::new("2-4,6-8");
+        assert_eq!(ep.check_for_any_overlap(), false);
+    }
+
+    #[test]
+    fn example_overlap_6() {
+        let ep = ElvesPair::new("2-3,4-5");
+        assert_eq!(ep.check_for_any_overlap(), false);
     }
 }
 
